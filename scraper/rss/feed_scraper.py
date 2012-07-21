0f28b672c12d1a1a7e4492c51be4fa57d776be8e
Adam Midvidy <amidvidy@gmail.com>
2012-07-21 15:55:11 -0400

    feed scraper is operational

--- scraper/rss/feed_scraper.py
+++ scraper/rss/feed_scraper.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
 
+import lxml.html
 import feedparser
 from pymongo import Connection
 import sys
 import time
+import md5
 
 # using old style objects, don't see any reason to use new style objects
 # as they are a bit slower and I don't need those features
@@ -14,6 +16,27 @@ class RssScraper:
         self.feeds = feeds
         self.limit = limit
         self.coll = coll
+    def scrape(self):
+        for feed in self.feeds:
+            parser = feedparser.parse(feed)
+            for entry in parser.entries:
+                url = entry.link
+                _id = md5.md5(url).hexdigest()
+                title = entry.title
+                wordcount = 0
+                # this block filters out all the html tags and counts
+                # the words
+                for content_part in entry.content:
+                    part = lxml.html.fromstring(content_part.value)
+                    part_text = part.text_content()
+                    part_word_list = part_text.split(" ")
+                    wordcount += len(part_word_list)
+                print(_id, url, title, wordcount)
+                self.coll.update(
+                    {"_id": _id},
+                    {"$set": {"title": title,
+                              "wordcount": wordcount}},
+                    upsert = True)
 
     
 def get_feeds(coll):
@@ -30,7 +53,7 @@ def main():
 
         # setup feed lists
         feeds = get_feeds(rss_source_coll)
-        print(feeds)
+
         # setup scraper
         scraper = RssScraper(
             feeds,
@@ -39,8 +62,8 @@ def main():
         ) 
         
         while True:
-           
-            time.sleep(60 * 5) # sleep for 5 mins
+            scraper.scrape()
+            time.sleep(60 * 10) # sleep for 10 mins
     except KeyboardInterrupt:
         print("Received keyboard interrupt. Exiting...")
         sys.exit(0)
